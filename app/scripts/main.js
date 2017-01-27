acdb7c507c0600acc96da89f54369df321173e85
zsolt nemeth <furion1@gmail.com>
2017-01-27 14:20:11 -0500

    harriet

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -60,250 +60,27 @@
       console.error('Error during service worker registration:', e);
     });
   }
-  let pages_container = $('.pages-container'),
-    full_page_wrap = $('.full-page-wrap'),
-    P1_wrap = $('#P1-wrap'),
-    ProjectMenu = $('#Project-Menu-Wrap'),
-    Project_Desc_Wrap = $('#Project-Desc-Wrap'),
-    projectCenter = $('.project-center-wrap'),
-    projectCategory = $('.project-category'),
-    ProjectUL = $('#Project-UL-List'),
-    XitTri = $('#Xit-Triangle'),
-    XitPlus = $('#XitPlus'),
-    inset_img = $('.inset-image'),
-    Project_BG = $('#Project-Desc-BG'),
-    active_desc = $('.project-desc-body'),
-    ctaViewProjects = $('.ctas a');
-  //GLOBAL TARGET VARIABLES
-  let targetProject,
-    //GLOBAL Timeline instances
-    desc_tl,
-    Expand_Down_tl;
-  let menu_tl = new TimelineLite({
-    paused: true,
-    force3D: true,
-    immediateRender: true,
-  }).set(ProjectUL, {
-    autoAlpha: 0,
-  }, 'label--0').set(Project_Desc_Wrap, {
-    autoAlpha: 1,
-    transformOrigin: "0 100",
-  }, 'label--0').set(active_desc.find('.h4-HR'), {
-    scaleX: 1,
-    transformOrigin: '0 0',
-  }, 'label--0').set(active_desc.find('a'), {
-    autoAlpha: 0,
-  }, 'label--0').set(XitPlus, {
-    color: 'rgb(61,61,70)',
-  }, 'label--0').to(ProjectMenu, 0.4, {
-    width: '83.333%',
-    bottom: 0,
-    backgroundColor: 'rgb(255,255,255)',
-    ease: Quad.easeInOut,
-    force3D: true,
-  }, 'label--2').to(ProjectMenu, 0.8, {
-    left: '16.666%',
-    height: '100%',
-    ease: Quint.easeInOut,
-    force3D: true,
-  }, 'label--2+=0.0').addLabel('tri-inMark', '-=0.5').addLabel('li-inMark', '-=0.35').to(XitTri, 0.9, {
-    top: '-9%',
-    right: '-11.25%',
-    rotation: '-45',
-    transformOrigin: '50,50',
-    backgroundColor: 'rgba(33,33,38,1.0)',
-    ease: Quint.easeInOut,
-    force3D: false,
-  }, 'tri-inMark-=0.5').to(XitPlus, 0.7, {
-    color: 'rgba(255,255,255,0.85)',
-    fontSize: '0.6em',
-    top: '4%',
-    right: '5.5%',
-    rotation: '-135',
-    ease: Quad.easeInOut,
-    force3D: true,
-  }, 'label--2').from(projectCenter, 1.35, {
-    scaleY: 0,
-    transformOrigin: '0% 100%',
-    ease: Quint.easeInOut,
-    force3D: true,
-  }, 'label--2-=0.2').from(Project_Desc_Wrap, 1.35, {
-    yPercent: 100,
-    ease: Quint.easeInOut,
-    force3D: true,
-  }, 'label--2-=0.1').from(projectCategory, 0.4, {
-    autoAlpha: 0,
-    ease: Cubic.easeOut,
-    force3D: true,
-  }, 'label--2+=0.6').to(ProjectUL, 0, {
-    autoAlpha: 1,
-  }, 'li-inMark+=0.15').staggerFrom('.project-li.upper', 0.4, {
-    autoAlpha: 0,
-    y: '100%',
-    scale: 1,
-    transformOrigin: '36.67% 100%',
-    ease: Back.easeOut,
-    force3D: true,
-  }, -0.03, 'li-inMark+=0.15').staggerFrom('.project-li.lower', 0.4, {
-    autoAlpha: 0,
-    y: '100%',
-    scale: 1,
-    transformOrigin: '36.67% 0%',
-    ease: Back.easeOut,
-    force3D: true,
-  }, -0.03, 'li-inMark').addLabel('label--3', '+=0.3').from(active_desc.find('h1'), 0.6, {
-    autoAlpha: 0,
-    y: '100%',
-    ease: Expo.easeOut,
-    force3D: true,
-  }, 'label--3-=0.35').from(active_desc.find('h3'), 0.6, {
-    y: '-225%',
-    ease: Expo.easeOut,
-    force3D: true,
-  }, 'label--3-=0.55').from(active_desc.find('h4'), 0.3, {
-    x: ' 510%',
-    ease: Expo.easeOut,
-    force3D: true,
-  }, 'label--3-=0.3').from(active_desc.find('.h4-HR'), 0.5, {
-    x: ' 75%',
-    ease: Expo.easeInOut,
-    force3D: true,
-  }, 'label--3-=0.7').to(active_desc.find('.h4-HR'), 0.9, {
-    scaleX: 0.25,
-    ease: Expo.easeOut,
-    force3D: true,
-  }, 'label--3-=0.3').from(active_desc.find('a'), 0.4, {
-    y: ' 200%',
-    height: '12.5%',
-    autoAlpha: 0,
-    ease: Circ.easeOut,
-    force3D: true,
-  }, 'label--3+=0.05').from(active_desc.find('h2'), 0.4, {
-    autoAlpha: 0,
-    y: 20,
-    ease: Circ.easeOut,
-    force3D: false,
-  }, 'label--3-=0.15').staggerFrom(active_desc.find('.pSpan_1 span'), 0.5, {
-    top: '8em',
-    ease: Quart.easeOut,
-    force3D: true,
-  }, 0.075, 'label--3-=0.3').staggerFrom(active_desc.find('.pSpan_1 span'), 0.45, {
-    autoAlpha: 0,
-    ease: Power2.easeInOut,
-    force3D: true,
-  }, 0.075, 'label--3-=0.3').to(active_desc.find('p'), 0.9, {
-    autoAlpha: 1,
-    ease: Circ.easeOut,
-    force3D: true,
-  }, 'label--3+=0.00') // opacity fade in for all p's, instead of staggering all. (project#1 staggers, others simply fade in.)
-    .staggerFrom(active_desc.find('h5'), 0.5, {
-      y: ' 200%',
-      autoAlpha: 0,
-      ease: Expo.easeOut,
-      force3D: true,
-    }, 0.1, 'label--3+=0.1').addLabel('snappy-reverse', '-=1.3');
-  let openProjects = () => {
-    // GLOBAL Target defined [Inital // First Project on.Open]
-    targetProject = $($(this).attr('data-target-project'));
-    ProjectMenu.toggleClass('active');
-    projectCenter.addClass('active');
-    Project_Desc_Wrap.toggleClass('active');
-    $('.First').addClass('active');
-    inset_img.toggleClass('active');
-    if (ProjectMenu.hasClass('active')) {
-      menu_tl.play().timeScale(1.0);
-    } else {
-      menu_tl.reverse('-=1.6');
-    }
-  }
-  XitTri.click(openProjects)
-  ctaViewProjects.click(openProjects)
-  $('.social-flag').click(openProjects)
-  var project_li = $('.project-li');
-  // Split h1 into characters for Stagger anim.
-  active_desc.each(function() {
-    $(this).find('h1').html($(this).find('h1').html().replace(/./g, '<span>$&</span>').replace(/\s/g, '&nbsp;'));
-  });
-  project_li.mouseenter(function() {
-    // GLOBAL Target defined
-    targetProject = $($(this).attr('data-target-project'));
-    desc_tl = new TimelineLite({
-      paused: true,
-      onCompleteParams: [targetProject.find('*')],
-      onComplete: endFix,
-    });
-    //  console.log(desc_tl);
-    desc_tl.set(targetProject.find('.h4-HR'), {
-      autoAlpha: 0.2,
-      scaleX: 1,
-      transformOrigin: '0 0',
-    }).set(targetProject.find('h1 span'), {
-      transformPerspective: '300',
-      transformOrigin: '50% 150%',
-      force3D: true,
-      transformStyle: "preserve-3d",
-    }).addLabel('label-0X', '+=0.0').staggerFrom(targetProject.find('h1 span'), 0.4, {
-      top: '-1em',
-      force3D: true,
-      transformStyle: 'preserve-3d',
-      ease: Power1.easeOut,
-      onCompleteParams: ['{self}'],
-    }, 0.02, 'label--1+=0.0').staggerFrom(targetProject.find('h1 span'), 0.55, {
-      rotationX: 90,
-      ease: Power4.easeOut,
-      force3D: true,
-      transformStyle: "preserve-3d",
-    }, 0.025, 'label--1+=0.1').from(targetProject.find('.desc-h1-cont'), 0.95, {
-      y: '-100%',
-      ease: Power3.easeOut,
-    }, 'label--2-=0.85').from(targetProject.find('h4'), 0.4, {
-      y: ' 100%',
-      ease: Power3.easeOut,
-      force3D: true,
-    }, 'label--2-=0.55').from(targetProject.find('h3'), 0.4, {
-      x: ' 206.7%',
-      ease: Expo.easeInOut,
-    }, 'label--2-=1').from(targetProject.find('.h4-HR'), 0.3, {
-      autoAlpha: 0.6,
-      x: ' 78%',
-      ease: Expo.easeOut,
-      force3D: true,
-    }, 'label--2-=0.6').to(targetProject.find('.h4-HR'), 0.6, {
-      scaleX: 0.25,
-      ease: Expo.easeOut,
-      force3D: true,
-    }, 'label--2-=0.4').from(targetProject.find('h2'), 0.6, {
-      width: '0%',
-      left: '+=8.333%',
-      ease: Expo.easeOut,
-      force3D: false,
-    }, 'label--2-=0.65').staggerFrom(targetProject.find('p > span'), 0.34, {
-      top: '6em',
-      ease: Sine.easeOut,
-      force3D: true,
-    }, 0.06, 'label--2-=0.8').staggerFrom(targetProject.find('p > span'), 0.34, {
-      autoAlpha: 0,
-      ease: Power2.easeInOut,
-    }, 0.06, 'label--2-=0.8');
-    if ($(this).hasClass('active')) {
-      desc_tl.progress(1.0);
-    } else {
-      $(this).addClass('active').siblings('li').removeClass('active');
-      $($(this).attr('data-target-project')).addClass('active').siblings().removeClass('active');
-      desc_tl.restart().timeScale(1.0);
-      Project_BG.css({
-        'background-color': $(this).attr('data-target-bg'),
-      });
-      XitPlus.css({
-        'color': $(this).attr('data-target-bg'),
-        'opacity': '0.8',
-      });
-    }
 
-    function endFix(x) {
-      TweenMax.set(x, {
-        clearProps: "all",
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    let mainTitle = $('.main-title')
+
+    // wait until window is loaded - meaning all images, style-sheets, and assets
+    window.onload = function() {
+
+    TweenMax.from(mainTitle, 0.5, {
+        autoAlpha: 0, 
+        xPercent: "-10", 
+        ease:Power4.easeOut,
+        delay: 0.3  
       });
+
     };
-  }); // Li-desc mouseenter END
+
+});
+  
+    
+    
+
 })();
